e310c17ce684fe2ac51fb32214db7de9b9fbfc8f
namgay tenzin <namgay.tenzin@dcpl.bt>
2022-01-25 11:41:07 +0600

    change route

--- routes.go
+++ routes.go
@@ -3,15 +3,18 @@ package main
 import "namgay/jampa/controllers"
 
 func initializeRoutes() {
-	router.GET("/campaigns", controllers.GetCampaigns)
-	router.GET("/campaigns/:id", controllers.GetCampaign)
-	router.POST("/campaigns", authenticateUser(), controllers.CreateCampaign)
-	router.PUT("/campaigns/:id", controllers.UpdateCampaign)
-	router.DELETE("/campaigns/:id", controllers.DeleteCampaign)
+	api := router.Group("/api")
+	{
+	api.GET("/campaigns", controllers.GetCampaigns)
+	api.GET("/campaigns/:id", controllers.GetCampaign)
+	api.POST("/campaigns", authenticateUser(), controllers.CreateCampaign)
+	api.PUT("/campaigns/:id", controllers.UpdateCampaign)
+	api.DELETE("/campaigns/:id", controllers.DeleteCampaign)
 
-	router.POST("/campaigns/:id/donate", controllers.CreateDonation)
-	router.GET("/campaigns/:id/donations", controllers.GetDonations)
+	api.POST("/campaigns/:id/donate", controllers.CreateDonation)
+	api.GET("/campaigns/:id/donations", controllers.GetDonations)
 
-	router.POST("/users", controllers.RegisterUser)
-	router.POST("/users/login", controllers.Login)
+	api.POST("/users", controllers.RegisterUser)
+	api.POST("/users/login", controllers.Login)
+	}
 }
